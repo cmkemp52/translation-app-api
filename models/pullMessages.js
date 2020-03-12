1b53d44d0dcb645bfedf70bfd62f1df5ba11a59a
Christopher Kemp <cmkemp52@gmail.com>
2020-03-12 01:26:11 -0400

    fixed translations

--- models/pullMessages.js
+++ models/pullMessages.js
@@ -2,18 +2,22 @@ const db = require("./conn");
 const translate = require("./translate.js");
 
 async function pullMessages(language, lastMessage = 0) {
-  messages = await db.any(
-    `SELECT * FROM chatroom WHERE id > ${lastMessage} LIMIT 50;`
-  );
-  if (messages.length > 0) {
-    for (message in messages) {
-      newMessage = await translate(language, messages[message].message);
-      console.log(messages[message]);
-      console.log(newMessage.data.translations[0].translatedText);
-      messages[message] = newMessage.data.translations[0].translatedText;
+  try {
+    messages = await db.any(
+      `SELECT * FROM chatroom WHERE id > ${lastMessage} LIMIT 50;`
+    );
+    if (messages.length > 0) {
+      for (message in messages) {
+        newMessage = await translate(language, messages[message].message);
+        console.log(messages[message]);
+        console.log(newMessage.data.translations[0].translatedText);
+        messages[message] = newMessage.data.translations[0].translatedText;
+      }
     }
+    return messages;
+  } catch (err) {
+    return err;
   }
-  return messages;
 }
 
 module.exports = pullMessages;
