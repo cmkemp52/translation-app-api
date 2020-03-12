812f92106c139851f8f8622a8b56e9ac50045f71
Christopher Kemp <cmkemp52@gmail.com>
2020-03-12 01:22:35 -0400

    fixed translations

--- models/pullMessages.js
+++ models/pullMessages.js
@@ -5,11 +5,10 @@ async function pullMessages(language, lastMessage = 0) {
   messages = await db.any(
     `SELECT * FROM chatroom WHERE id > ${lastMessage} LIMIT 50;`
   );
-  console.log(messages);
   if (messages.length > 0) {
     for (message in messages) {
       newMessage = await translate(language, messages[message].message);
-      console.log(newMessage);
+      console.log(newMessage.data.translations[0].translatedText);
       messages[message].message =
         newMessage.data.translations[0].translatedText;
     }
