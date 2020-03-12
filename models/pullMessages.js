24044165e5289f33d78d6141cc525535234d1562
Christopher Kemp <cmkemp52@gmail.com>
2020-03-12 01:06:54 -0400

    added translation to chat

--- models/pullMessages.js
+++ models/pullMessages.js
@@ -1,10 +1,15 @@
 const db = require("./conn");
+const translate = require("./translate.js");
 
 async function pullMessages(language, lastMessage = 0) {
   messages = await db.any(
     `SELECT * FROM chatroom WHERE id > ${lastMessage} [LIMIT { 50 }] ;`
   );
   console.log(messages);
+  for (message in messages) {
+    newMessage = await translate(language, messages[message].message);
+    messages[message] = newMessage;
+  }
   return messages;
 }
 
